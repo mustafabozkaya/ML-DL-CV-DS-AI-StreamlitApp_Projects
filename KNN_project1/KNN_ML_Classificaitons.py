1a7b9259a8d53c851e8be4cf7ef18557110ebe69
mustafa <mustafaegebozkaya555@hotmail.com>
2021-09-27 11:54:04 +0300

    imported necessary library and read Data file

---
+++ KNN_project1/KNN_ML_Classificaitons.py
@@ -0,0 +1,65 @@
+import pandas as pd # for data manipulation and analysis. In particular, it offers data structures and operations for manipulating numerical tables and time series
+pd.set_option('display.max_columns', None) # set pd.options.display features
+pd.set_option('display.max_rows', None)
+import numpy as np # for multi array manipÃ¼lation process,Linear algebra
+import seaborn as sns # for data visulations,its very usefully
+import matplotlib.pyplot as plt # for data visulations
+plt.rcParams["figure.figsize"]=(18,8)
+from matplotlib.colors import ListedColormap # for color palette for plots
+sns.set_theme(style="darkgrid")
+from sklearn import utils
+from sklearn.preprocessing import StandardScaler #for normalization and data scaler
+from sklearn.model_selection import train_test_split, GridSearchCV # fro trainins,test ,validaitons ana model hyperparameters tuning
+from sklearn.metrics import accuracy_score, confusion_matrix,f1_score,classification_report # for models evaulations 
+from sklearn.neighbors import KNeighborsClassifier, LocalOutlierFactor # for model build, outlier detections
+from sklearn.decomposition import PCA # for dimentional reductions .Principle compomnet Analys
+from sklearn.datasets import load_breast_cancer # for datasets 
+# closed warning library
+import warnings
+warnings.filterwarnings("ignore")
+
+# include data 
+try:
+    
+    data = pd.read_csv("KNN_project/breast_cancer.csv")
+    
+except Exception as e:
+    
+    print(type(e))
+    print(e)
+    #from import datasets from sklearn if CSV FİLE İS NOT FOUND
+    data=load_breast_cancer()
+
+#create df  if CSV FİLE İS NOT FOUND
+if type(data)==utils.Bunch:
+    df=np.concatenate((data.target.reshape(-1,1),data.data), axis=1)
+    data=pd.DataFrame(df,columns=np.insert(data.feature_names,0,"diagnosis"))
+else:
+    # remove unnecesssary columns
+    data.drop(['Unnamed: 32','id'], inplace = True, axis = 1)
+
+data = data.rename(columns = {"diagnosis":"Dependent"})
+data_=data.copy()
+sns.countplot(y=data_["Dependent"],
+                   linewidth=5,
+                   edgecolor=sns.color_palette("dark", 3))
+print(data.Dependent.value_counts())
+
+if not data.Dependent.dtype in [np.float64,np.int64]:
+    
+    data["Dependent"] = [0 if i.strip() == "M" else 1 for i in data.Dependent]
+
+elif  data.Dependent.dtype in [np.float64,np.int64]:
+    data_["Dependent"] = ["Malignant" if i==0 else  "Belign"  for i in data.Dependent]
+
+print(len(data))
+
+print(data.head())
+
+print("Data shape ", data.shape)
+
+data.info()
+
+describe = data.describe()
+
+columns_features=data.columns.to_numpy()
---
+++ README.md
@@ -0,0 +1,58 @@
+# **Streamlt Aplications with Machine Learning & Deep learnings Projects  )**
+I am plannig to develop  machine learning and Deep learnings algorthm projects  that works based variety contunious,categorical  data.Each  project is about of clustering and regresion, estimation,classificaiton. I will use The most known algorithm for  projects is that there is a lot of data and works with different accuracy metrics. finally I will develop an application that will allow you to test every project using streamlit api.  The software automatically brings you the predicitons from the background data with the selection of columns and rows.
+
+ See you new projects!
+
+
+
+
+
+
+
+### **The coding language use:**
+
+'Python 3.8.5'
+
+!["python"](https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=Python&logoColor=darkgreen)
+
+### **Libraries Used:**
+
+
+![](https://img.shields.io/badge/scikit_learn-F7931E?style=for-the-badge&logo=scikit-learn&logoColor=white)
+
+![](https://img.shields.io/badge/Numpy-777BB4?style=for-the-badge&logo=numpy&logoColor=white)
+
+![](https://img.shields.io/badge/Pandas-2C2D72?style=for-the-badge&logo=pandas&logoColor=white)
+
+![](https://img.shields.io/badge/Plotly-239120?style=for-the-badge&logo=plotly&logoColor=white)
+
+`Matplotlib`
+
+![](https://img.shields.io/badge/Streamlit-FF4B4B?style=for-the-badge&logo=Streamlit&logoColor=white)
+
+### **Data Source:**
+
+[![kaggle](https://img.shields.io/badge/Kaggle-20BEFF?style=for-the-badge&logo=Kaggle&logoColor=white)](https://www.kaggle.com/datasets?fileType=csv&datasetsOnly=true)
+
+### **Information links:**
+
+
+## Contact (Email) :
+ [![Mail Badge](https://img.shields.io/badge/info.mustafabozkaya@gmail.com-c14438?style=for-the-badge&logo=Gmail&logoColor=white&link=mailto:info.mustafabozkaya@gmail.com)](mailto:info.mustafabozkaya@gmail.com)
+
+## Official Web site :
+
+[![website](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://mustafabozkaya.github.io/)
+
+
+## Kaggle:
+
+[![kaggle](https://img.shields.io/badge/Kaggle-20BEFF?style=for-the-badge&logo=Kaggle&logoColor=white)](https://www.kaggle.com/mustafabozka)
+
+
+## Linkedin :
+
+[![linkedin](https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/mustafa-bozkaya/)
+
+
+
